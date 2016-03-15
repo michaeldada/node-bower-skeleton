6a9adb8d867ded00f1dc3c6b78e5b1b78814eec2
David <ddiehr@yahoo.com>
2016-03-15 11:52:22 -0700

    add alarm functionality

---
+++ js/alarmClock.js
@@ -0,0 +1,4 @@
+exports.alarmClock = function(hour, minute){
+  var dateTimeObject = new Date(2016, 2, 15, hour, minute);
+  return dateTimeObject;
+};
--- js/time-interface.js
+++ js/time-interface.js
@@ -1,3 +1,46 @@
+var alarmClock = require('./../js/alarmClock.js').alarmClock;
+var datetime = null, date = null;
+var update = function () {
+  date = moment(new Date());
+  datetime.html(date.format('h:mm:ss a'));
+};
+
+function alarmCheck(time){
+  date = new Date();
+  date = Date.parse(date);
+  if (time == date){
+    $('#alarm').css('background-color', 'red');
+  }
+  console.log(typeof time);
+  console.log("**"+time);
+  console.log(typeof date);
+  console.log("*"+date+"*");
+}
+
 $(document).ready(function(){
-  $('#time').text(moment().format("MMM Do YYYY"));
+    datetime = $('#time');
+
+    var alarmTime;
+
+    $('#form').submit(function(event){
+      event.preventDefault();
+      var hour = $('#hour').val();
+      var minute = $('#minute').val();
+      var alarm = alarmClock(hour, minute);
+
+      $('#alarm').text(alarm);
+      alarmTime = Date.parse(alarm);
+      console.log(alarmTime+"***");
+      alarmCheck(alarmTime);
+      setInterval(alarmCheck, 1000, alarmTime);
+    });
+
+
+    update();
+    setInterval(update, 1000);
+
+
+
+
+
 });
